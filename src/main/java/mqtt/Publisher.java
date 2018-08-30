de87660c837ce670d7b28b3cbbb4f057d1ffeebd
Joao Mota Jr <joaomotajr@hotmail.com>
2018-08-30 00:02:10 -0300

    Fix Bugs

--- src/main/java/mqtt/Publisher.java
+++ src/main/java/mqtt/Publisher.java
@@ -11,17 +11,18 @@ public class Publisher {
 
   public static void Execute(String broker, String id, String value) throws MqttException {
     
-	  MainApp.logger.error("START Publisher => => Broker :: " + broker );
+	  MainApp.logger.info("START Publisher => => Broker :: " + broker );
 	  
 	  MqttClient client = new MqttClient(broker, MqttClient.generateClientId());    
+//	  client.setCallback( new MqttCallBack("127.0.0.1") );
 	  client.connect();
     
 	  MqttMessage message = new MqttMessage();
-	  message.setPayload(value.getBytes());
-    
+	  message.setPayload(value.getBytes());    
 	  client.publish(id, message);
-
 	  client.disconnect();
+	  
+	  MainApp.logger.info("END Publisher." );
 
   	}
 }
